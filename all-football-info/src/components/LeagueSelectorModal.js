a63a4bc380b5efc6743f7f56068b3cf02ca1734b
Nunomatos7 <84334113+Nunomatos7@users.noreply.github.com>
2024-11-26 09:46:10 +0000

    Update LeagueSelectorModal.js

--- all-football-info/src/components/LeagueSelectorModal.js
+++ all-football-info/src/components/LeagueSelectorModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { useDispatch } from "react-redux";
 import { setLeague } from "../redux/slices/leagueSlice";
